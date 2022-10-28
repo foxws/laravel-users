c3ef5684eb6204e55fc4af6b6542a89ea364c176
FoxWS <111247253+foxws@users.noreply.github.com>
2022-10-28 15:06:30 +0200

    Update users.php

--- config/users.php
+++ config/users.php
@@ -3,7 +3,7 @@
 return [
     'user_model' => \Foxws\Users\Models\User::class,
 
-    'teams_model' => \Foxws\Users\Models\Team::class,
+    'team_model' => \Foxws\Users\Models\Team::class,
 
     'usergroup_model' => \Foxws\Users\Models\Usergroup::class,
 ];
